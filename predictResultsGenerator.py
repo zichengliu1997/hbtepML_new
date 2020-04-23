7a1a46609a7d67d3aec49abb15d041e2876eb5c5
zichengliu1997 <51381871+zichengliu1997@users.noreply.github.com>
2020-04-22 22:18:19 -0400

    Updated

--- predictResultsGenerator.py
+++ predictResultsGenerator.py
@@ -2,6 +2,7 @@ from tensorflow.keras.models import load_model
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 look_back = 10
 model = load_model('LSTM_model.h5')
@@ -26,17 +27,7 @@ for filename in os.listdir(datapath):
 #%%
 predictions.to_pickle("predictions.pkl")
 #%%
-from tensorflow.keras.models import load_model
-import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-look_back = 10
-model = load_model('LSTM_model.h5')
-datapath = 'C:/Users/Zicheng Liu/Documents/GitHub/hbtepML/Test data/'
-timepath = 'C:/Users/Zicheng Liu/Documents/GitHub/hbtepML/Test data time/'
-filename = "100007.npy"
+filename = "105841.npy"
 os.path.join(datapath, filename)
 test_data_np = np.load(os.path.join(datapath, filename))
 time = np.load(os.path.join(timepath, 'time_'+filename))
@@ -44,10 +35,15 @@ results = model.predict(test_data_np)
 results = results.transpose()
 probability = results[1]
 t = time[look_back:]
+threshold_np = np.full((len(t),),0.75)
+idx = np.argwhere(np.diff(np.sign(threshold_np - probability))).flatten()
 plt.figure()
-plt.plot(t*1000,probability)
-plt.title("Disruptiion Probability (P(t)) vs. Time from Plasma Breakdown (t)\nShot 100007")
+plt.plot(t*1000,probability, label = 'Disruptivity')
+plt.axhline(0.75, c='k', label = "Threshold")
+plt.scatter(t[idx[0]]*1000, threshold_np[idx[0]], c = "r")
+plt.title("Disruptiion Probability (P(t)) vs. Time from Plasma Breakdown (t)\nShot 105841")
 plt.xlabel("t(ms)")
 plt.ylabel("P(t)")
 plt.grid()
+plt.legend()
 plt.show()
